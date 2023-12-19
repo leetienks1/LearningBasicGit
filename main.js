b38f29141eb47438ae0635a85dba5e094b090193
leetienks1 <tien.le@omnibox.com.vn>
2023-12-19 23:44:24 +0700

    firstJS

---
+++ main.js
@@ -0,0 +1,15 @@
+// alert('day là thông báo')
+
+// console.log('chu')
+
+// confirm('bạn có đủ 18 tuổi không')
+
+// prompt('nhập tuổi của bạn')
+
+// setTimeout(function(){
+//     alert(' đăng ký đê')
+// },1000)
+
+// setInterval(function(){
+//     alert('đăng ký')
+// } , 1000)
